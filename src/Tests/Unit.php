57d47f69edd52ba5e3d2ec7e32534f9826810e45
Babichev Maxim <info@babichev.net>
2017-09-25 13:10:47 +0300

    update unit

--- src/Tests/Unit.php
+++ src/Tests/Unit.php
@@ -7,4 +7,15 @@ use PHPUnit\Framework\TestCase;
 class Unit extends TestCase
 {
 
+    /**
+     * @var Tmp
+     */
+    protected $tmp;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->tmp = new Tmp();
+    }
+
 }
